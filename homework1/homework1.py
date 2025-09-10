1aa00fbd5987b6bd86ffffce59ab8c0bba803910
Andrew Gately <agately12@berkeley.edu>
2025-09-10 16:06:44 -0700

    Initial commit

---
+++ homework1/homework1.py
@@ -0,0 +1,53 @@
+# File: homework1. py
+
+a = 10
+print(a)
+print(type(a)) # a is an integer, a whole number with no decimals
+
+b = 1.5
+print(b)
+print(type(b)) # b is a float, it includes decimals
+
+c = 3j
+print(c)
+print(type(c))
+
+d = "hello"
+print(d)
+print(type(d))
+
+e = [1, 2, 3]
+print(e)
+print(type(e))
+
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+print(f)
+print(type(f))
+
+g = (1, 2)
+print(g)
+print(type(g))
+
+h = ["apple", "banana", "strawberry"]
+print(h)
+print(type(h))
+
+i = True
+print(i)
+print(type(i))
+
+j = None
+print(j)
+print(type(j))
+
+k = [True, "blue", 12]
+print(k)
+print(type(k))
+
+l = str(14)
+print(l)
+print(type(l))
+
+m = 1e4
+print(m)
+print(type(m))
