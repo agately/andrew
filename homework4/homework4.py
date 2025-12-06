d4b115a143094df1d18851084edaa86f916f61f9
Andrew Gately <agately12@berkeley.edu>
2025-12-05 18:18:39 -0800

    done with hw4

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -1,126 +1,129 @@
 import random
 
+favorite_food = ["pizza", "burritos", "pancakes", "steak", "rice"]
 
-# favorite_food = ["pizza", "burritos", "pancakes", "steak", "rice"]
+print(favorite_food[1])
 
-# print(favorite_food[1])
+print(favorite_food[-1])
 
-# print(favorite_food[-1])
+favorite_food.append("chicken")
 
-# favorite_food.append("chicken")
+print(favorite_food)
 
-# print(favorite_food)
+favorite_food.insert(0, "apple")
 
-# favorite_food.insert(0, "apple")
+print(favorite_food)
 
-# print(favorite_food)
+favorite_food.remove("burritos")
 
-# favorite_food.remove("burritos")
+print(favorite_food)
 
-# print(favorite_food)
+print(len(favorite_food))
 
-# print(len(favorite_food))
+def print_upper(list):
+    for i in range(len(list)):
+        print(list[i].upper())
 
-# def print_upper(list):
-#     for i in range(len(list)):
-#         print(list[i].upper())
+print_upper(favorite_food)
 
-# print_upper(favorite_food)
+print(favorite_food[0:6:5])
 
-# print(favorite_food[0:6:5])
+def contains_potato(list):
+    b = False
 
-# def contains_potato(list):
-#     b = False
-
-#     for i in range(len(list)):
-#         if list[i] == "potato":
-#             b = True
+    for i in range(len(list)):
+        if list[i] == "potato":
+            b = True
         
-#     if b == True:
-#         return print("A potato!")
-#     else:
-#         return print("No potato!")
+    if b == True:
+        return print("A potato!")
+    else:
+        return print("No potato!")
 
     
-# foods = ["broccoli"]  
+foods = ["broccoli"]  
 
-# print(foods)
-# contains_potato(foods)
-
-
+print(foods)
+contains_potato(foods)
 
 
-# numbers = list(range(20))
 
 
-# def get_first_15(numbers):
-#     return numbers[:15]
+numbers = list(range(20))
 
-# def get_every_5th(numbers):
-#     return numbers[::5] 
 
-# def reverse_and_stride(numbers):
-#     list = numbers[::-1]
-#     return list[::3]
+def get_first_15(numbers):
+    return numbers[:15]
 
-# step1 = get_first_15(numbers)
-# step2 = get_every_5th(step1)
-# step3 = reverse_and_stride(step2)
+def get_every_5th(numbers):
+    return numbers[::5] 
 
-# print(step1)
-# print(step2)
-# print(step3)
+def reverse_and_stride(numbers):
+    list = numbers[::-1]
+    return list[::3]
 
-# numbers = [
-#     [1, 2, 3],
-#     [4, 5, 6],
-#     [7, 8, 9]
-# ]
+step1 = get_first_15(numbers)
+step2 = get_every_5th(step1)
+step3 = reverse_and_stride(step2)
 
-# print(numbers[2])
+print(step1)
+print(step2)
+print(step3)
 
-# print(numbers[1][1])
+numbers = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
-# numbers.append([11,12,13])
+print(numbers[2])
 
-# print(numbers)
+print(numbers[1][1])
 
-# def sum_nested(list):
-#     rows = len(list)
-#     cols = len(list[0])
-#     i = 0
-#     total = 0 
+numbers.append([11,12,13])
 
-#     while i < rows:
-#         total = total + sum(list[i])
-#         i = i + 1
+print(numbers)
+
+def sum_nested(list):
+    rows = len(list)
+    cols = len(list[0])
+    i = 0
+    total = 0 
+
+    while i < rows:
+        total = total + sum(list[i])
+        i = i + 1
     
-#     return total
+    return total
 
 
-# my_list = [
-#     [1,2],
-#     [3,4]
-# ]
+my_list = [
+    [1,2],
+    [3,4]
+]
 
-# sum = sum_nested(my_list)
+sum = sum_nested(my_list)
 
-# print(sum)
+print(sum)
 
 
-# def five_by_five():
-#     list = []
+def five_by_five():
+    list = []
     
 
-#     for i in range(5):
-#         row = []
-#         for j in range(5):
-#             num = random.randint(1, 25)
-#             row.append(num)
+    for i in range(5):
+        row = []
+        for j in range(5):
+            num = random.randint(1, 25)
+            row.append(num)
         
-#         list.append(row)
+        list.append(row)
 
-#     return list
+    return list
+
+my_list = five_by_five()
+
+print(my_list)
 
 # notes on problem 3.4: I created a a function that generates a random list of numbers between 1 and 25
 # Were they supposed to be random or not?
@@ -134,19 +137,26 @@ ages = {
     "Mira": 48
 }
 
-# print(ages["Katie"])
+print(ages["Katie"])
 
-# ages["Mira"] = 100
+ages["Mira"] = 100
 
-# print(ages["Mira"])
+print(ages["Mira"])
 
-# ages["Milana"] = 52
+ages["Milana"] = 52
 
-# print(ages)
+print(ages)
 
-# del ages["Milana"]
+del ages["Milana"]
 
-# print(ages)
+print(ages)
+
+for person in ages:
+    print(person, "is", ages[person], "years old")
+
+
+
+# Include code at the bottom of your file that calls this function and prints the result
 
 for person in ages:
     print(person, "is", ages[person], "years old")
